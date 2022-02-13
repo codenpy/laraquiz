f3e3232933395f36647e40db75b90b78acfa5809
Noor E Alam <codenpy@gmail.com>
2022-02-13 06:05:26 +0600

    changes files and migrations

---
+++ app/Models/SelectedOption.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SelectedOption extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'quiz_id',
+        'question_id',
+        'selected_option',
+    ];
+}
